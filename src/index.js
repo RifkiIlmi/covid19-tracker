538e1aff9dec09384630529f632feb26e40b3796
rifkiilmi <rifkiilmi98@gmail.com>
2020-09-01 08:40:04 +0700

    First Config

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import 'bootstrap'
+import '../src/styles/scss/app.scss'
+import './styles/style.css'
---
+++ webpack.common.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+ 
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            /* style and css loader */
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        // Loader for webpack to process CSS with PostCSS
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function () {
+                            return [
+                                // require('precss'),
+                                require('autoprefixer')
+                            ];
+                            }
+                        }
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    /* plugin */
+    plugins: [
+        /* HTML Webpack Plugin */
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "index.html"
+        })
+    ]
+}
